5f8f3eca1fa79a3a2a0139e3597aab86f85b98a6
Mohammed Alrushdy <48268026+m7md7sam@users.noreply.github.com>
2020-12-14 21:33:52 +0300

    implemented firebase utiles, included ability to store authenticated users int our firebase database

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from './components/header/Header';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/SignInAndSignUp';
 import Homepage from './pages/homepage/Homepage';
 import ShopPage from './pages/shop/ShopPage';
-import {auth} from './firebase/firebase';
+import {auth, createUserProfileDocument} from './firebase/firebase';
 
 class App extends Component {
   constructor(props) {
@@ -20,8 +20,21 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapshot => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          });
+          console.log(this.state)
+        });
+      } else {
+        this.setState({currentUser: userAuth})
+      }
     });
   }
 
