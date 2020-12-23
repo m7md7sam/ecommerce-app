021f7dcacf859a7c5218bd088228f30e3979dffc
Mohammed Alrushdy <48268026+m7md7sam@users.noreply.github.com>
2020-12-23 03:52:32 +0300

    save cartItems in the Local Store, by using Redux persist library

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,19 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
 
 import userReducer from './userReducer';
 import cartReducer from './cartReducer';
 
-export default combineReducers({
+const presistCongig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+};
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 });
+
+export default persistReducer(presistCongig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
 import reducer from './reducers';
@@ -7,4 +8,6 @@ const middlewares = [logger];
 
 const store = createStore(reducer, applyMiddleware(...middlewares));
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
